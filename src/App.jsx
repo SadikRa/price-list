35ac649793feb13f2d6f7cec80dd77a83f4ec1f6
SadikRa <sadikrahman494@gmail.com>
2024-09-18 20:19:20 +0600

    phone chart added done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 
 import './App.css'
+import LineChart from './components/LineChart/LineChart'
 import Navbar from './components/Navbar/Navbar'
+import Phone from './components/Phone/Phone'
 import PriceOptions from './components/PriceOptions/PriceOptions'
 
 
@@ -12,6 +14,8 @@ function App() {
     <div className='container mx-auto'>
     <Navbar></Navbar>
     <PriceOptions></PriceOptions>
+    <LineChart></LineChart>
+    <Phone></Phone>
     </div>
     </>
   )
